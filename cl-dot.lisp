8346c1113e8291285bdad064df612d155f415d5e
Nick Patrick <npatrick04@gmail.com>
2015-06-22 21:28:46 -0500

    Get some attributes working.

--- cl-dot.lisp
+++ cl-dot.lisp
@@ -27,25 +27,38 @@
     (labelurl . "labelURL")
     (tailurl . "tailURL")))
 
-(defun symbol->attr (symbol)
+(defun symbol->id (symbol)
   "Given a symbol for an attribute, return its ID."
-  (let ((special-case (assoc symbol special-ids)))
-    (if special-case
-        (cdr special-case)
-        (string-downcase (symbol-name symbol)))))
+  (etypecase symbol
+    (symbol (let ((special-case (assoc symbol special-ids)))
+              (if special-case
+                  (cdr special-case)
+                  (string-downcase (symbol-name symbol)))))
+    (string symbol)))
 
 (defun id->symbol (id)
   "Given an attribute ID, return a symbol."
-  (let ((special-case (rassoc id special-ids)))
-    (if special-case
-        (car special-case)
-        (intern (string-upcase id) :cl-dot))))
+  (etypecase id
+    (string (let ((special-case (rassoc id special-ids)))
+              (if special-case
+                  (car special-case)
+                  (intern (string-upcase id) :cl-dot))))
+    (symbol id)))
 
 (defclass identified ()
   ((id :reader id :initarg :id :initform nil :type (or nil id-type))))
 
 (defclass attributed ()
-  ((attributes :accessor attributes :initarg :attributes)))
+  ((attributes :accessor attributes :initarg :attributes :initform nil)))
+
+(defun print-alist (alist stream &optional (bracket t))
+  (when alist
+    (format stream "~:[~;[~]~{~A~^, ~}~:[~;]~]"
+            bracket
+            (loop for (attr . value) in alist
+               collect (format nil "~{~A=\"~A\"~}"
+                               (mapcar #'symbol->id (list attr value))))
+            bracket)))
 
 (defun normalize-statement (statement)
   (etypecase statement
@@ -56,31 +69,38 @@
                                               (subseq statement 0 arrow))
                                              (normalize-statement
                                               (subseq statement (+ 2 arrow)))))
-           (make-instance 'node :id statement))))
+           (make-instance 'node :id (string-downcase statement)))))
+    (cons
+     (print-alist (list statement) nil nil))
     (symbol
      (normalize-statement (symbol-name statement)))))
 
 (defclass subgraph (identified)
-  ((node-attrs :accessor node-attrs :initarg :node-attrs)
-   (edge-attrs :accessor node-attrs :initarg :node-attrs)
-   (statements :accessor statements :initarg :statements)))
-(defmethod :after initialize-instance ((object subgraph) &key &allow-other-keys)
-  (mapc #'normalize-statement statements))
+  ((node-attrs :accessor node-attrs :initarg :node-attrs :initform nil)
+   (edge-attrs :accessor edge-attrs :initarg :edge-attrs :initform nil)
+   (statements :accessor statements :initarg :statements :initform nil)))
+(defmethod initialize-instance :after ((object subgraph) &key &allow-other-keys)
+  (setf (statements object) (mapcar #'normalize-statement (statements object))))
 
 (defmethod print-object ((object subgraph) stream)
   (format stream
           "~:[~;strict ~]~:[~:[subgraph~;graph~]~;~*digraph~] ~:[~;~:*~A ~]~%~
-           {~%~{  ~A;~%~}}~%"
+           {~%~
+             ~:[~;  ~:*node ~A;~%~]~
+             ~:[~;  ~:*edge ~A;~%~]~
+             ~{  ~A;~%~}~
+           }~%"
           (graph-strict object)
           (typep object 'digraph)
           (typep object 'graph)
           (when (id object) (string-downcase (id object)))
+          (when (node-attrs object) (print-alist (node-attrs object) nil))
+          (when (edge-attrs object) (print-alist (edge-attrs object) nil))
           (statements object)))
 
 ;;; A graph uses --
 (defclass graph (subgraph)
-  ((strict :reader graph-strict :initarg :strict :initform nil :type (or nil t))
-   (identifiers :accessor identifiers :initform (make-hash-table))))
+  ((strict :reader graph-strict :initarg :strict :initform nil :type (or nil t))))
 
 ;;; A digraph uses ->
 (defclass digraph (graph)
@@ -88,15 +108,15 @@
 
 (defclass node (identified attributed)
   ())
+;; (defmethod initialize-instance :after ((object node) &key &allow-other-keys)
+;;; Perhaps set a reference to a hash of all nodes...
+;;   (setf (get (id->symbol (id object)) 'node) object))
 
 (defmethod print-object :after ((object attributed) stream)
-  (format stream "[~{~A~^, ~}]"
-          (loop for (attr . value) in (attributes object)
-               collect (format nil "~A=\"~A\"" attr value))))
+  (print-alist (attributes object) stream))
 
 (defmethod print-object ((object node) stream)
-  (format stream "~A"
-          (id object)))    ;TODO: add attributes
+  (format stream "~A" (string-downcase (id object))))
 
 (defun node-or-subgraph? (element)
   (or (typep element 'node)
@@ -114,8 +134,8 @@
 
 (defmethod print-object ((object edge) stream)
   (format stream "~A -> ~A"
-          (id (car (nodes object)))
-          (id (cadr (nodes object)))))    ;TODO: add attributes
+          (string-downcase (id (car (nodes object))))
+          (string-downcase (id (cadr (nodes object)))))) ;TODO: add attributes
 
 (defun dot-read (str)
   (string-trim-left " \n" str))
